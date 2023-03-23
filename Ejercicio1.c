f1b409cf7dab805ca87e7dff3f7e827aa576de0b
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2023-03-23 14:37:25 -0600

    Primer ejercicio

---
+++ Ejercicio1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+  {
+    printf("%d\n", 455);
+    printf("%i\n", 455);
+    printf("%d\n", +455);
+    printf("%d\n", -455);
+    printf("%hd\n", 32000);
+    printf("%ld\n", 20000000000);
+    printf("%o\n", 455);
+    printf("%u\n", 455);
+    printf("%u\n", -455);
+    printf("%x\n", 455);
+    printf("%X\n", 455);
+
+    return 0;
+}
